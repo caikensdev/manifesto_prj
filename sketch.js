43b6c3e0411c312960eda993892660572146627d
Caleb <calebaikens@gmail.com>
2021-12-07 12:49:49 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  // Create the canvas
+
+  createCanvas(windowWidth, 3000);
+
+
+  // Set colors
+  fill(204, 101, 192, 127);
+  stroke(127, 63, 120);
+
+}
+
+function draw() {
+
+  if (mouseIsPressed){
+    fill(0);
+      } else {
+          fill(255);
+      }
+
+  translate(mouseX, mouseY);
+
+
+  for (let i = 0; i < 10; i ++) {
+    ellipse(0, 20, 10, 40);
+    rect(0, 0, 0, 15);
+    rotate(PI/5)
+    ellipse(0,0,15,15);
+
+  }
+}
